a93b2746c2e4b9d9867f61893723efbccc259502
Edward Zhang <edwardz123@gmail.com>
2021-11-18 22:15:17 -0800

    Add score

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import RingToTileXYArray from 'phaser3-rex-plugins/plugins/board/board/ring/RingToTileXYArray'
 import { Fly } from '~/lib/Fly'
 import { Player } from '~/lib/Player'
 
@@ -7,6 +8,9 @@ export default class Game extends Phaser.Scene {
   public flies!: Phaser.GameObjects.Group
   public static GAME_WIDTH = 800
   public static GAME_HEIGHT = 600
+
+  private score = 0
+  private scoreText
 
   constructor() {
     super('game')
@@ -17,8 +21,18 @@ export default class Game extends Phaser.Scene {
       classType: Fly,
     })
     this.flies.add(new Fly(this, 100, 100).sprite)
+    this.flies.add(new Fly(this, 100, 100).sprite)
+    this.flies.add(new Fly(this, 100, 100).sprite)
+    this.flies.add(new Fly(this, 100, 100).sprite)
     this.player = new Player(this)
     this.cameras.main.setBackgroundColor('#99CCFF')
+    this.scoreText = this.add.text(16, 16, 'score: 0', {
+      fontSize: '32px',
+    })
+    this.player.onScored.push((points) => {
+      this.score += points
+      this.scoreText.setText('Score: ' + this.score)
+    })
   }
 
   update() {
