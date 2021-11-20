7e91ccb4d1b10bf244ce137c09b0104aaf1927a1
Edward Zhang <edwardz123@gmail.com>
2021-11-19 23:06:25 -0800

    add fly animation (#3)

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -6,8 +6,11 @@ export default class Preloader extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('fly', 'fly.png')
     this.load.image('pirahna-plant', 'pirahna-plant.png')
+    this.load.spritesheet('fly', 'fly-Sheet.png', {
+      frameWidth: 32,
+      frameHeight: 32,
+    })
   }
   create() {
     this.scene.start('game')
