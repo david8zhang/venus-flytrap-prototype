9497ea2734e33737747a61b4585010003317ec59
David Zhang <david8zhang@berkeley.edu>
2021-11-21 18:43:52 -0800

    added venus flytrap eating

--- src/lib/Player.ts
+++ src/lib/Player.ts
@@ -1,13 +1,15 @@
+import { Constants } from '~/util/constants'
+import Game from '../scenes/Game'
 import GameScene from '../scenes/Game'
 import { Fly } from './Fly'
 
 export class Player {
   private scene: GameScene
-  private headSprite: Phaser.Physics.Arcade.Sprite
+  // private headSprite: Phaser.Physics.Arcade.Sprite
 
-  // Default position
-  private defaultX: number = GameScene.GAME_WIDTH / 2
-  private defaultY: number = GameScene.GAME_HEIGHT - 50
+  private playerGroup!: Phaser.Physics.Arcade.Group
+  private sprites: Phaser.Physics.Arcade.Sprite[] = []
+  public isMovingSprites: boolean[] = []
 
   public isMoving = false
 
@@ -16,40 +18,107 @@ export class Player {
 
   constructor(scene: GameScene) {
     this.scene = scene
+    this.createAnims()
+    this.addSprites()
+    this.configureKeyPresses()
+  }
 
-    this.headSprite = scene.physics.add.sprite(0, 0, 'pirahna-plant')
-    this.headSprite.setScale(0.25)
-    this.headSprite.setPosition(this.defaultX, this.defaultY)
-    this.scene.physics.world.enableBody(
-      this.headSprite,
-      Phaser.Physics.Arcade.DYNAMIC_BODY
-    )
+  createAnims(): void {
+    this.scene.anims.create({
+      key: 'plant-bite',
+      frames: this.scene.anims.generateFrameNames('plant-bite', {
+        start: 0,
+        end: 2,
+        suffix: '.png',
+      }),
+      frameRate: 8,
+    })
+  }
 
-    this.scene.input.on(
-      'pointerup',
-      (pointer) => {
-        if (!this.isMoving) {
-          this.isMoving = true
-          const clickedX = pointer.x
-          const clickedY = pointer.y
-          this.scene.time.delayedCall(500, () => {
-            this.headSprite.setPosition(clickedX, clickedY)
-            this.scene.time.delayedCall(500, () => {
-              this.headSprite.setPosition(this.defaultX, this.defaultY)
-              this.isMoving = false
-            })
-          })
-        }
-      },
-      this
-    )
+  addSprites(): void {
+    this.playerGroup = this.scene.physics.add.group()
+    const xSpread = Constants.GAME_WIDTH / 4
 
+    for (let i = 1; i <= 3; i++) {
+      const sprite = this.scene.physics.add.sprite(
+        i * xSpread,
+        Constants.GAME_HEIGHT - 32,
+        'plant',
+        0
+      )
+      sprite.setScale(2)
+      this.playerGroup.add(sprite)
+      this.sprites.push(sprite)
+    }
     this.scene.getEnemyGroups().forEach((group: Phaser.GameObjects.Group) => {
-      this.scene.physics.add.overlap(this.headSprite, group, (obj1, obj2) => {
-        const fly = obj2.getData('ref') as Fly
+      this.scene.physics.add.overlap(this.playerGroup, group, (obj1, obj2) => {
+        const fly = obj1.getData('ref') as Fly
         fly.destroy()
         this.onScored.forEach((handler) => handler(1))
       })
     })
+    this.isMovingSprites = new Array(3).fill(false)
+  }
+
+  configureKeyPresses(): void {
+    const keyCodeMapping = ['KeyQ', 'KeyW', 'KeyE']
+    this.scene.input.keyboard.on('keydown', (keyCode: any) => {
+      const spriteToMoveIndex = keyCodeMapping.indexOf(keyCode.code)
+      if (
+        spriteToMoveIndex !== -1 &&
+        !this.isMovingSprites[spriteToMoveIndex]
+      ) {
+        this.isMovingSprites[spriteToMoveIndex] = true
+        const spriteToMove = this.sprites[spriteToMoveIndex]
+        this.moveSprite(this.sprites[spriteToMoveIndex], () => {
+          this.isMovingSprites[spriteToMoveIndex] = false
+        })
+      }
+    })
+  }
+
+  moveSprite(sprite: Phaser.Physics.Arcade.Sprite, onComplete: Function): void {
+    const yDiff = sprite.y - Constants.SPAWN_THRESHOLD.upper - 20
+    const timeline = this.scene.tweens.createTimeline()
+    sprite.setDepth(1)
+    const stem = this.scene.physics.add
+      .sprite(sprite.x, sprite.y + 5, 'stem')
+      .setScale(2)
+      .setDepth(0)
+
+    const timeline2 = this.scene.tweens.createTimeline()
+    timeline2.add({
+      targets: stem,
+      scaleY: '+=32',
+    })
+    timeline2.add({
+      targets: stem,
+      scaleY: '-=32',
+      delay: 1000,
+    })
+
+    timeline.add({
+      targets: sprite,
+      y: `-=${yDiff}`,
+      onComplete: () => {
+        sprite.anims.play({ key: 'plant-bite', repeat: 0 })
+        sprite.body.enable = false
+      },
+    })
+    timeline.add({
+      targets: sprite,
+      delay: 1000,
+      y: `+=${yDiff}`,
+      onComplete: () => {
+        this.scene.time.delayedCall(1000, () => {
+          stem.destroy()
+          sprite.body.enable = true
+          sprite.setTexture('plant', 0)
+          onComplete()
+        })
+      },
+    })
+    timeline2.play()
+    timeline.play()
   }
 }
