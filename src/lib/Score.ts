e38f4717c238aee8a5bbab203eee6cbb6725068f
David Zhang <david8zhang@berkeley.edu>
2021-11-30 00:20:15 -0800

    Added health bar (#6)
    * added venus flytrap eating
    * added healthbar
    * added healthbar
    * fix healthbar negative bug, add colors for pie status

--- src/lib/Score.ts
+++ src/lib/Score.ts
@@ -1,52 +1,23 @@
 import { Player } from './Player'
 import Game from '~/scenes/Game'
-import { Fly } from './Fly'
-import { Spawner } from './Spawner'
 
-export const MAX_HEALTH = 20
 export class Score {
   private scene: Game
 
   private score = 0
   private scoreText: Phaser.GameObjects.Text
 
-  public health = MAX_HEALTH
-  private healthText: Phaser.GameObjects.Text
-
-  public onHealthDecreased: Array<() => void> = []
-
-  constructor(scene: Game, player: Player, spawners: Spawner[]) {
+  constructor(scene: Game, player: Player) {
     this.scene = scene
 
-    this.scoreText = this.scene.add.text(16, 16, 'Score: 0', {
-      fontSize: '32px',
+    this.scoreText = this.scene.add.text(16, 10, 'Score: 0', {
+      fontSize: '24px',
     })
-    this.healthText = this.scene.add.text(16, 64, `Health: ${MAX_HEALTH}`, {
-      fontSize: '32px',
-    })
-
     player.onScored.push(this.increaseScore.bind(this))
-
-    spawners.forEach((spawner: Spawner) => {
-      spawner.enemies.children.entries.forEach((child) => {
-        const fly: Fly = child.getData('ref') as Fly
-        fly.onReachedRightEdge.push(this.decreaseHealth.bind(this))
-      })
-
-      spawner.onNewEnemy.push((fly) =>
-        fly.onReachedRightEdge.push(this.decreaseHealth.bind(this))
-      )
-    })
   }
 
   increaseScore(points: number): void {
     this.score += points
     this.scoreText.setText('Score: ' + this.score)
-  }
-
-  decreaseHealth(): void {
-    this.health -= 1
-    this.healthText.setText('Health: ' + this.health)
-    this.onHealthDecreased.forEach((handler) => handler())
   }
 }
